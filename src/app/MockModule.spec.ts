4c5ee4d3f63a60f11bb2a7a3d00a5f8c6ad8e48d
Joshua Neuenhoff <joshua@neuenhoff.net>
2020-09-09 19:27:54 -0400

    Add example of MockProvider

--- src/app/MockModule.spec.ts
+++ src/app/MockModule.spec.ts
@@ -1,6 +1,6 @@
 import { Injectable, NgModule } from "@angular/core";
 import { TestBed } from '@angular/core/testing';
-import { MockModule } from 'ng-mocks';
+import { MockModule, MockProvider } from 'ng-mocks';
 import { ngMocksUniverse } from 'ng-mocks/dist/lib/common/ng-mocks-universe';
 
 describe('for services inside a module', () => {
@@ -34,6 +34,48 @@ describe('for services inside a module', () => {
     });
 
     it('should not be able to pass state between tests (setup phase)', () => {
+        expect(exampleProvider.a).toBeUndefined();
+
+        // Set a property on the class to see if it carries over to the next test.
+        exampleProvider.a = 11;
+
+        testOrderValidationFlag = true;
+    });
+
+    it('should not be able to pass state between tests (validation phase)', () => {
+        expect(testOrderValidationFlag).toBeTrue();
+        expect(exampleProvider.a).toBeUndefined();
+    });
+});
+
+describe('for services mocked directly', () => {
+    @Injectable()
+    class ExampleProvider {
+      a = 5;
+    }
+
+    let exampleProvider: ExampleProvider;
+
+    // Test order is important, since we are testing side effects between tests. A random order would result in false positives.
+    // To detect these, the setup phase sets this flag, and the validation phase checks that it was set.
+    let testOrderValidationFlag = false;
+
+    beforeAll(() => {
+        // Uncommenting this line allows the tests to pass.
+        // ngMocksUniverse.flags.delete('cacheProvider');
+    });
+
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            providers: [MockProvider(ExampleProvider)],
+        });
+
+        exampleProvider = TestBed.inject(ExampleProvider);
+    });
+
+    it('should not be able to pass state between tests (setup phase)', () => {
+        expect(exampleProvider.a).toBeUndefined();
+
         // Set a property on the class to see if it carries over to the next test.
         exampleProvider.a = 11;
 
